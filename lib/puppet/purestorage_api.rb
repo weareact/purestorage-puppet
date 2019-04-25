a946bed8be0370cc540ba5da8c316331b7977dd2
Jon Law <jonathan.law@weareact.com>
2019-04-25 15:50:38 +0100

    Fix URI reference

--- lib/puppet/purestorage_api.rb
+++ lib/puppet/purestorage_api.rb
@@ -48,7 +48,7 @@ class PureStorageApi
 
   def make_rest_api_call(request, session_header = true, parse_response = true)
     # Create the HTTP objects
-    http             = Net::HTTP.new(uri.host, uri.port)
+    http             = Net::HTTP.new(request.uri.host, request.uri.port)
     http.use_ssl     = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
