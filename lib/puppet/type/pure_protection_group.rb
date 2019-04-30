ccd21ee61abc4ac80e0ab3f3de7ecb9f0fc0e331
Jon Law <jonathan.law@weareact.com>
2019-04-30 17:19:54 +0100

    new value vs newvalues

--- lib/puppet/type/pure_protection_group.rb
+++ lib/puppet/type/pure_protection_group.rb
@@ -50,42 +50,42 @@ Puppet::Type.newtype(:pure_protection_group) do
 
   newparam(:snapshot_enabled) do
     desc "If snapshot schedule is enabled"
-    newvalue(:true, :false)
+    newvalues(:true, :false)
   end
 
   newparam(:snapshot_frequency_unit) do
     desc "The unit of time snapshot frequency is defined as: minutes, hours, days"
-    newvalue(:minutes, :hours, :days)
+    newvalues(:minutes, :hours, :days)
   end
 
   newparam(:snapshot_frequency_amount) do
     desc "How frequent snapshots are scheduled"
-    newvalue(%r{\\d+})
+    newvalues(%r{\\d+})
   end
 
   newparam(:snapshot_at) do
     desc "Number of seconds after midnight that a daily snapshot is taken"
-    newvalue(%r{\\d+})
+    newvalues(%r{\\d+})
   end
 
   newparam(:snapshot_retention_unit) do
     desc "The unit of time snapshot retention is defined as: minutes, hours, days"
-    newvalue(:minutes, :hours, :days)
+    newvalues(:minutes, :hours, :days)
   end
 
   newparam(:snapshot_retention_amount) do
     desc "How long snapshots are kept for"
-    newvalue(%r{\\d+})
+    newvalues(%r{\\d+})
   end
 
   newparam(:snapshot_per_day) do
     desc "How many snapshots are kept for the given number of days"
-    newvalue(%r{\\d+})
+    newvalues(%r{\\d+})
   end
 
   newparam(:snapshot_for_days) do
     desc "How many days retained snapshots are kept for"
-    newvalue(%r{\\d+})
+    newvalues(%r{\\d+})
   end
 
   newparam(:device_url) do
