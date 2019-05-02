73484fd4936ab6a67ebaba1037b4add7239e669f
Jon Law <jonathan.law@weareact.com>
2019-05-02 14:40:12 +0100

    Formatting hash

--- lib/puppet/util/network_device/pure/facts.rb
+++ lib/puppet/util/network_device/pure/facts.rb
@@ -21,12 +21,12 @@ class Puppet::Util::NetworkDevice::Pure::Facts
 
     controller_info = Purest::PhysicalArray.get(controllers: true)
     Puppet.debug("Returned controller info: #{controller_info.inspect}")
-    controller_hash             = controller_info.map {|controller| [controller[:name], controller.delete_if {|k, v| k == :name}]}.to_h
+    controller_hash             = convert_array_hash_to_hash_array(controller_info, :name)
     @pure_storage[:controllers] = controller_hash
 
     connection_info = Purest::Host.get(connect: true)
     Puppet.debug("Returned connection info: #{connection_info.inspect}")
-    connection_hash             = connection_info.map {|connection| [connection[:name], connection.delete_if {|k, v| k == :name}]}.to_h
+    connection_hash             = convert_array_hash_to_hash_array(connection_info, :name)
     @pure_storage[:connections] = connection_hash
 
     Puppet.debug("Got facts: #{@facts.inspect}")
@@ -34,5 +34,17 @@ class Puppet::Util::NetworkDevice::Pure::Facts
     @facts[:pure_storage] = @pure_storage
 
     @facts
+  end
+
+  def convert_array_hash_to_hash_array(array, hash_key)
+    result = {}
+    array.each do |i|
+      name    = i[hash_key]
+      current = result[name] ||= []
+      current << i.delete_if {|k, v| k == hash_key}
+      result[name] = current
+    end
+
+    result
   end
 end
