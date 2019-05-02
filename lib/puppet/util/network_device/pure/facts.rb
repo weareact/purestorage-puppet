6cc48a1653bf3d1fd5af7ab219fd1f35c78012d1
Jon Law <jonathan.law@weareact.com>
2019-05-02 12:38:18 +0100

    Update fact format

--- lib/puppet/util/network_device/pure/facts.rb
+++ lib/puppet/util/network_device/pure/facts.rb
@@ -9,23 +9,28 @@ class Puppet::Util::NetworkDevice::Pure::Facts
   end
 
   def retrieve
-    @facts = {}
-    @facts['vendor_id']   = 'pure'
+    @facts                    = {}
+    @pure_storage             = {}
+    @pure_storage[:vendor_id] = 'pure'
 
     Puppet.debug("Fetching facts from Pure Array")
     array_info = Purest::PhysicalArray.get
     Puppet.debug("Returned array info: #{array_info.inspect}")
-    @facts['array_name']  = array_info[:array_name]
-    @facts['version']     = array_info[:version]
+    @pure_storage[:array_name] = array_info[:array_name]
+    @pure_storage[:version]    = array_info[:version]
 
     controller_info = Purest::PhysicalArray.get(controllers: true)
     Puppet.debug("Returned controller info: #{controller_info.inspect}")
-    @facts['controllers'] = controller_info
+    controller_hash             = controller_info.map {|controller| [controller[:name], controller.delete_if {|k, v| k == :name}]}.to_h
+    @pure_storage[:controllers] = controller_hash
 
     connection_info = Purest::Host.get(connect: true)
     Puppet.debug("Returned connection info: #{connection_info.inspect}")
-    @facts['connections'] = connection_info
+    connection_hash             = connection_info.map {|connection| [connection[:name], connection.delete_if {|k, v| k == :name}]}.to_h
+    @pure_storage[:connections] = connection_hash
+
     Puppet.debug("Got facts: #{@facts.inspect}")
-    @facts
+
+    @facts[:pure_storage] = @pure_storage
   end
 end
