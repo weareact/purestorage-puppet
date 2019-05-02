235b98d75bfb64ec93349fe1d6934f14511c02b9
Jon Law <jonathan.law@weareact.com>
2019-05-02 11:40:59 +0100

    Add missing setcode nesting

--- lib/facter/pure-facts.rb
+++ lib/facter/pure-facts.rb
@@ -1,25 +1,28 @@
 require 'purest'
 
 Facter.add(:pure_storage) do
-  @facts              = {}
-  @facts['vendor_id'] = 'pure'
+  setcode do
 
-  Puppet.debug("Fetching facts from Pure Array")
-  array_info = Purest::PhysicalArray.get
-  Puppet.debug("Returned array info: #{array_info.inspect}")
-  @facts['array_name'] = array_info[:array_name]
-  @facts['version']    = array_info[:version]
+    @facts              = {}
+    @facts['vendor_id'] = 'pure'
 
-  controller_info = Purest::PhysicalArray.get(controllers: true)
-  Puppet.debug("Returned controller info: #{controller_info.inspect}")
-  @facts['controllers'] = controller_info
+    Puppet.debug("Fetching facts from Pure Array")
+    array_info = Purest::PhysicalArray.get
+    Puppet.debug("Returned array info: #{array_info.inspect}")
+    @facts['array_name'] = array_info[:array_name]
+    @facts['version']    = array_info[:version]
 
-  connection_info = Purest::Host.get(connect: true)
-  Puppet.debug("Returned connection info: #{connection_info.inspect}")
-  @facts['connections'] = connection_info
-  Puppet.debug("Got facts: #{@facts.inspect}")
+    controller_info = Purest::PhysicalArray.get(controllers: true)
+    Puppet.debug("Returned controller info: #{controller_info.inspect}")
+    @facts['controllers'] = controller_info
 
-  @facts
+    connection_info = Purest::Host.get(connect: true)
+    Puppet.debug("Returned connection info: #{connection_info.inspect}")
+    @facts['connections'] = connection_info
+    Puppet.debug("Got facts: #{@facts.inspect}")
+
+    @facts
+  end
 end
 
 
